40e226050d31a210f375c6093af1091c17ca26b9
pengincoalition <kage2.kage22@gmail.com>
2021-07-19 00:20:55 +0900

    Make test

---
+++ Renban_test.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"testing"
+)
+
+func TestMake_URL_by_index(t *testing.T) {
+	type T1 struct {
+		k1, v string
+		k2    int
+	}
+	test_data := []T1{
+		T1{k1: "http://example.com/imgs/*.jpg", k2: 1, v: "http://example.com/imgs/1.jpg"},
+		T1{k1: "http://example.com/imgs/***.jpg", k2: 1, v: "http://example.com/imgs/1.jpg"},
+	}
+
+	for _, e := range test_data {
+		result := Make_URL_by_index(e.k1, e.k2)
+		if result != e.v {
+			t.Errorf("err %s %d->%s  But %s", e.k1, e.k2, e.v, result)
+		}
+	}
+
+}
